73e4f9ebea8a5c590b4f65742311b22e48a44799
mewu <mewu@idmod.org>
2019-06-14 16:18:43 -0700

    update based on info in https://circleci.com/gh/ckan/ckan/5062#config/containers/1

--- contrib/windows-local/setup_test.cmd
+++ contrib/windows-local/setup_test.cmd
@@ -7,26 +7,62 @@ pip install -r test-requirements.txt
 cd ../../
 python setup.py develop
 
-REM Create test databases
+REM Create test db and users
 docker exec -it db adduser --disabled-password --gecos "" ckan
 docker exec -it db runuser -l ckan -c "createdb -O ckan ckan_test -E utf-8"
-docker exec -it db runuser -l ckan -c "createdb -O ckan datastore_test -E utf-8"
 
-REM Create datastore users
-docker exec -it db runuser ckan -c "psql -c \"CREATE USER ckan_read WITH PASSWORD 'ckan';\""
-docker exec -it db runuser ckan -c "psql -c \"GRANT USAGE ON SCHEMA public TO ckan_read;\""
-docker exec -it db runuser ckan -c "psql -c \"GRANT SELECT ON ALL TABLES IN SCHEMA public TO ckan_read;\""
+REM Create datastore db users
+docker exec -it db runuser -l ckan -c "psql -c \"CREATE USER datastore_read WITH PASSWORD 'pass' NOSUPERUSER NOCREATEDB NOCREATEROLE;\""
+docker exec -it db runuser -l ckan -c "psql -c \"CREATE USER datastore_write WITH PASSWORD 'pass' NOSUPERUSER NOCREATEDB NOCREATEROLE;\""
+docker exec -it db runuser -l ckan -c "createdb -O datastore_write datastore_test -E utf-8"
 
-REM install npm packages
-START /wait CMD /c npm install -g mocha-phantomjs@3.5.0 phantomjs@~1.9.1
+cd contrib/windows-local
+REM initialize db and set permission (todo: make it work)
+REM ECHO off
 
-cd contrib/windows-local  
-ECHO OFF
-ECHO please follow the steps below:
-ECHO run: paster serve test-core.ini
-ECHO open a new cmd and run: mocha-phantomjs http://localhost:5000/base/test/index.html
-ECHO cd ../../ then run the command below in the virtualenv that you created:
-ECHO nosetests -v --ckan --ckan-migration --reset-db  --with-pylons=contrib/windows-local/test-core.ini -d ckan ckanext
+REM SETLOCAL EnableDelayedExpansion
+REM   for /f "usebackq tokens=*" %%a in ('paster datastore set-permissions -c test-core.ini') do ( set var=!var!%%x)
+REM ENDLOCAL
+REM ECHO ON
+REM ECHO %var%
+
+docker exec -it db runuser -l ckan -c "psql -c \"%var%\""
+paster db init -c test-core.ini
+
+START paster serve test-core.ini
+TIMEOUT 10
+
+REM install npm packages and run ui tests
+START /wait CMD /c npm install -g mocha-phantomjs@3.5.0 phantomjs@~1.9.1 mocha-xunit-reporter
+mocha-phantomjs http://localhost:5000/base/test/index.html -R xunit>test_mocha.xml
+
+cd ../../
+nosetests -v --ckan --ckan-migration --reset-db  --with-pylons=test-core.ini -x --with-coverage --cover-package=ckan --cover-package=ckanext --with-xunit --xunit-file=ckan_test.xml ckan ckanext
 ECHO if you want to run individual tests add --tests for example: --tests=TestAppDispatcher.test_ask_around_flask_core_and_pylons_extension_route
 ECHO add --with-html --html-report=test.html for better reporting
-ECHO ON
+
+
+REM stop web server (todo: find a better way to do it)
+taskkill /IM python.exe /F
+
+
+TIMEOUT 10
+REM cleanup 
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM datastore_read;\""
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM datastore_read;\""
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM datastore_read;\""
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE USAGE ON SCHEMA public FROM datastore_read;\""
+docker exec -it db runuser -l ckan -c "psql -c \"DROP USER datastore_read;\""
+
+
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM datastore_write;\""
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM datastore_write;\""
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM datastore_write;\""
+docker exec -it db runuser -l ckan -c "psql -c \"REVOKE USAGE ON SCHEMA public FROM datastore_write;\""
+docker exec -it db runuser -l ckan -c "psql -c \"DROP USER datastore_write;\""
+
+docker exec -it db runuser -l ckan -c "dropdb ckan_test"
+docker exec -it db runuser -l ckan -c "dropdb datastore_test"
+
+cd contrib/windows-local
+START paster serve development.ini
