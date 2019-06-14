6a76a5448ad57b952071f5286f6369cd3969fe84
MeWu-IDM <40474438+MeWu-IDM@users.noreply.github.com>
2019-06-14 11:24:36 -0700

    fix test conmand

--- contrib/windows-local/setup_test.cmd
+++ contrib/windows-local/setup_test.cmd
@@ -23,10 +23,10 @@ START /wait CMD /c npm install -g mocha-phantomjs@3.5.0 phantomjs@~1.9.1
 cd contrib/windows-local  
 ECHO OFF
 ECHO please follow the steps below:
-ECHO run paster serve test-core.ini
+ECHO run: paster serve test-core.ini
 ECHO open a new cmd and run: mocha-phantomjs http://localhost:5000/base/test/index.html
-ECHO run the command below in the virtualenv that you created:
-ECHO nosetests -v --ckan --ckan-migration --reset-db -d ckan ckanext
-ECHO if you want to run individual tests add --tests=TestAppDispatcher.test_ask_around_flask_core_and_pylons_extension_route
+ECHO cd ../../ then run the command below in the virtualenv that you created:
+ECHO nosetests -v --ckan --ckan-migration --reset-db  --with-pylons=contrib/windows-local/test-core.ini -d ckan ckanext
+ECHO if you want to run individual tests add --tests for example: --tests=TestAppDispatcher.test_ask_around_flask_core_and_pylons_extension_route
 ECHO add --with-html --html-report=test.html for better reporting
 ECHO ON
